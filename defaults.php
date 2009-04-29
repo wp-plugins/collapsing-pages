dd499eb650fd00aaafe90d29b53990e3b33dbb51
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-29 22:22:23 +0000

    added defaults.php
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-pages/tags/0.5.alpha@114430 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ defaults.php
@@ -0,0 +1,16 @@
+<?php
+      $defaults=array(
+        'title' => __('Pages', 'collapsing-pages'), 
+        'sortOrder'=> 'ASC' ,
+        'sort'=> 'pageName' ,
+        'defaultExpand'=> '',
+        'expand' => '1',
+        'depth' =>-1,
+        'inExcludePage' => 'include',
+        'linkToPage' => true,
+        'inExcludePages' => '',
+        'showPosts' => false,
+        'animate' => 0,
+        'postTitleLength' => 0
+      );
+?>
