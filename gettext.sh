1f6f4ca1491b960ede8e86149ee94adc7c564f1b
Bernhard Reiter <Bernhard Reiter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-20 15:53:24 +0000

    Removed translation tools -- just use poEdit instead. Updated German translations
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-pages/trunk@185347 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gettext.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xgettext --keyword=__ --keyword=_e  --default-domain=collapsing-pages --language=php *.php
-mv collapsing-pages.po collapsing-pages.pot
-
--- process_po_files.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-msgfmt -o collapsing-pages-de_DE.mo collapsing-pages-de_DE.po
